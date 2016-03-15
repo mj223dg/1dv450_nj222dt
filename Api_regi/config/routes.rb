1f185f0689e79b022091c46b6580691fbb791670
nj222dt <ni.johannesson@gmail.com>
2016-03-15 17:04:11 +0000

    Added the first resources and controller

--- Api_regi/config/routes.rb
+++ Api_regi/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+    namespace :api, defaults: {format: "json"} do
+    namespace :v1 do
+      resources :positions, only: [:index, :create, :show, :update, :destroy]
+      resources :tags
+      get "tag/specific" => "tags#show_specific_event"
+    end
+  end
   root  'home#index'
   get 'signup' => 'users#new'
   get 'create' => 'apis#new'
