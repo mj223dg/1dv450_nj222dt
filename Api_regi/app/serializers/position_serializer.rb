b594a65f207878ba1f17277cc7af4f26d581f1ae
nj222dt <ni.johannesson@gmail.com>
2016-03-17 16:55:05 +0000

    Forgot a S

--- Api_regi/app/serializers/position_serializer.rb
+++ Api_regi/app/serializers/position_serializer.rb
@@ -2,7 +2,7 @@ class PositionSerializer < ActiveModel::Serializer
   attributes :id, :address, :longitude, :latitude, :links
   def links
     {
-      school: api_v1_position_school_path(object.id),
+      schools: api_v1_position_schools_path(object.id),
     }
   end
 end
